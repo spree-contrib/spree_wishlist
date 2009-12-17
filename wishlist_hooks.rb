77c1230537c565e1b64d1976bbdf09db346a587b
David North <davidnorth@railsdog.com>
2009-12-17 21:26:12 +0000

    update hooks to new erb style

--- wishlist_hooks.rb
+++ wishlist_hooks.rb
@@ -3,7 +3,9 @@ class WishlistHooks < Spree::ThemeSupport::HookListener
   insert_after :cart_form, 'products/wishlist_form'
   insert_after :account_my_orders, 'users/wishlists'
   insert_after :wishlist_footer_links do
-    content_tag('div', link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)), :id => 'email_to_friend')
+    %(<div id="email_to_friend">
+      <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
+    </div>)
   end
   
 end
