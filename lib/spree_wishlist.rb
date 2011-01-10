710105a83570369ceeec15ad044d04b34c90152f
Steven Jenkins <steven.jenkins@gmail.com>
2011-01-10 12:05:07 -0800

    register the ability

--- lib/spree_wishlist.rb
+++ lib/spree_wishlist.rb
@@ -10,6 +10,7 @@ module SpreeWishlist
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+      Ability.register_ability(WishlistAbility)
     end
 
     config.to_prepare &method(:activate).to_proc
