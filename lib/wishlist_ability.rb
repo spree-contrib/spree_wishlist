b2c276f0f2ffac7b46948edab1c8e2efbdfec987
Steven Jenkins <steven.jenkins@gmail.com>
2011-01-11 13:43:40 -0500

    polished up CanCan rules

--- lib/wishlist_ability.rb
+++ lib/wishlist_ability.rb
@@ -4,13 +4,18 @@ class WishlistAbility
 
   def initialize(user)
 # TODO - this is really soggy (i.e., non-DRY), and it's not all necessary
+    # You can only index your own wishlists
     can :index, Wishlist do |wishlist|
       wishlist.user == user
     end
-    can :create, Wishlist do |wishlist|
-      wishlist.user == user
-    end
+    # Anyone can create a wishlist
+    can :create, Wishlist
+    # You can only see your own wishlist
     can :show, Wishlist do |wishlist|
+      wishlist.user == user || !wishlist.is_private?
+    end
+    # You can only change your own wishlist
+    can :move, Wishlist do |wishlist|
       wishlist.user == user
     end
     can :update, Wishlist do |wishlist|
@@ -18,6 +23,26 @@ class WishlistAbility
     end
     can :edit, Wishlist do |wishlist|
       wishlist.user == user
+    end
+
+    can :destroy, Wishlist do |wishlist|
+      wishlist.user == user
+    end
+
+    can :create, WishedProduct do |wished_product|
+      wished_product.wishlist.user == user
+    end
+    can :index, WishedProduct do |wished_product|
+      wished_product.wishlist.user == user
+    end
+    can :show, WishedProduct do |wished_product|
+      wished_product.wishlist.user == user || wished_product.wishlist.is_public?
+    end
+    can :delete, WishedProduct do |wished_product|
+      wished_product.wishlist.user == user
+    end
+    can :update, WishedProduct do |wished_product|
+      wished_product.wishlist.user == user
     end
   end
 end
