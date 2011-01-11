58b4b23cb038f13966bb642b7636e4e20fb30eab
Steven Jenkins <steven.jenkins@gmail.com>
2011-01-11 07:08:06 -0800

    refer to send_to_friend, not the parent

--- lib/spree_wishlist_hooks.rb
+++ lib/spree_wishlist_hooks.rb
@@ -3,7 +3,7 @@ class SpreeWishlistHooks < Spree::ThemeSupport::HookListener
   insert_after :account_my_orders, 'users/wishlists'
   insert_after :wishlist_footer_links do
     %(<div id="email_to_friend">
-      <%= link_to(t('email_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
+      <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
     </div>)
   end
 end
