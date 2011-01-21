5c37c880beca771014b612ac52e1c852029f441c
Roman Smirnov <roman@railsdog.com>
2011-01-21 15:10:21 +0300

    Improved I18n support

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -4,7 +4,7 @@ User.class_eval do
   def wishlist
     default_wishlist = self.wishlists.first(:conditions => ["is_default = ?", true]) 
     default_wishlist ||= self.wishlists.first
-    default_wishlist ||= self.wishlists.create(:name => "My wishlist", :is_default => true)
+    default_wishlist ||= self.wishlists.create(:name => I18n.t(:default_wishlist_name, :default => "My wishlist"), :is_default => true)
     default_wishlist.update_attribute(:is_default, true) unless default_wishlist.is_default?
     default_wishlist
   end
