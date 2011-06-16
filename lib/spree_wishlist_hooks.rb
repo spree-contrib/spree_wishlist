cf9ca567cabaa7586919f94fb8008f9797858b96
Brian Quinn <briandquinn@gmail.com>
2011-06-16 13:52:16 +0100

    Deface overrides tweaks

--- lib/spree_wishlist_hooks.rb
+++ lib/spree_wishlist_hooks.rb
@@ -1,19 +1,17 @@
-class SpreeWishlistHooks < Spree::ThemeSupport::HookListener
-  Deface::Override.new(:virtual_path => "products/show",
-                       :name => "converted_cart_form_445784258",
-                       :insert_after => "[data-hook='cart_form'], #cart_form[data-hook]",
-                       :partial => "products/wishlist_form",
-                       :disabled => false)
-  Deface::Override.new(:virtual_path => "users/show",
-                       :name => "converted_account_my_orders_389006052",
-                       :insert_after => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
-                       :partial => "users/wishlists",
-                       :disabled => false)
-  Deface::Override.new(:virtual_path => "wishlists/show",
-                       :name => "converted_wishlist_footer_links",
-                       :insert_after => "[data-hook='wishlist_footer_links'], #footer_links[data-hook]",
-                       :text => %(<div id="email_to_friend">
-                         <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
-                       </div>),
-                       :disabled => false)
-end
+Deface::Override.new(:virtual_path => "products/show",
+                     :name => "add_wish_to_cart_form",
+                     :insert_bottom => "[data-hook='cart_form']",
+                     :partial => "products/wishlist_form",
+                     :disabled => false)
+Deface::Override.new(:virtual_path => "users/show",
+                     :name => "add_wishlists_to_account_my_orders",
+                     :insert_after => "[data-hook='account_my_orders']",
+                     :partial => "users/wishlists",
+                     :disabled => false)
+Deface::Override.new(:virtual_path => "wishlists/show",
+                     :name => "add_email_to_wishlist_footer_links",
+                     :insert_after => "[data-hook='wishlist_footer_links']",
+                     :text => %(<div id="email_to_friend">
+                       <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
+                     </div>),
+                     :disabled => false)
