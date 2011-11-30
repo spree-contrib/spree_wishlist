234c736142c1f329d20e9e875ffb286c925d6e79
John Dyer <lbrapid@gmail.com>
2011-11-30 15:52:15 -0500

    Added migration to prefix tables with spree

---
+++ db/migrate/20111130204731_prefix_tables_with_spree.rb
@@ -0,0 +1,6 @@
+class PrefixTablesWithSpree < ActiveRecord::Migration
+  def change
+    rename_table :wishlists, :spree_wishlists
+    rename_table :wished_products, :spree_wished_products
+  end
+end
