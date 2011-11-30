2f26563d9c2341c064ef487d8318a317e42bca54
John Dyer <lbrapid@gmail.com>
2011-11-30 15:43:01 -0500

    Namespace and Refactor lib directory a bit

---
+++ lib/spree_wishlist/engine.rb
@@ -0,0 +1,20 @@
+module SpreeWishlist
+  class Engine < Rails::Engine
+    engine_name 'spree_wishlist'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+
+      #Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/**/*.rb")) do |c|
+      #  Rails.application.config.cache_classes ? require(c) : load(c)
+      #end
+      Spree::Ability.register_ability(WishlistAbility)
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end
--- lib/wishlist_ability.rb
+++ lib/wishlist_ability.rb
@@ -4,28 +4,28 @@ class WishlistAbility
 
   def initialize(user)
     # Anyone can create a wishlist
-    can :index, Wishlist do
+    can :index, Spree::Wishlist do
       !user.new_record?
     end
 
-    can :create, Wishlist
+    can :create, Spree::Wishlist
     # You can your own wishlists, and everyone cas see public ones
-    can :show, Wishlist do |wishlist|
+    can :show, Spree::Wishlist do |wishlist|
       wishlist.user == user || wishlist.is_public?
     end
     # You can only change your own wishlist
-    can [:show, :move, :update, :edit, :destroy], Wishlist do |wishlist|
+    can [:show, :move, :update, :edit, :destroy], Spree::Wishlist do |wishlist|
       wishlist.user == user
     end
 
-    can :create, WishedProduct do |wished_product|
+    can :create, Spree::WishedProduct do |wished_product|
       !user.new_record?
     end
 
-    can :show, WishedProduct do |wished_product|
+    can :show, Spree::WishedProduct do |wished_product|
       wished_product.wishlist.user == user || wished_product.wishlist.is_public?
     end
-    can [:index, :update, :delete], WishedProduct do |wished_product|
+    can [:index, :update, :delete], Spree::WishedProduct do |wished_product|
       wished_product.wishlist.user == user
     end
   end
