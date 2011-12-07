d490b94ce6f441434fc4f50a1f1bbe8e16b0cd43
John Dyer <lbrapid@gmail.com>
2011-12-07 13:52:30 -0500

    Rename namespace migration

--- db/migrate/20111130204731_prefix_wishlist_tables_with_spree.rb
+++ db/migrate/20111130204731_prefix_wishlist_tables_with_spree.rb
@@ -1,4 +1,4 @@
-class PrefixTablesWithSpree < ActiveRecord::Migration
+class PrefixWishlistTablesWithSpree < ActiveRecord::Migration
   def change
     rename_table :wishlists, :spree_wishlists
     rename_table :wished_products, :spree_wished_products
