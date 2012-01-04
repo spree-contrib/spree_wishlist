c6a21ddabb0f0fa88ca62276cc8226200fdcf706
John Dyer <lbrapid@gmail.com>
2012-01-04 12:29:51 -0500

    Added model specs

--- spec/models/spree/wishlist_spec.rb
+++ spec/models/spree/wishlist_spec.rb
@@ -2,10 +2,74 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Spree::Wishlist do
   before(:each) do
-    @wishlist = Spree::Wishlist.new(:name => 'test')
+    @user = Factory(:user)
+    @wishlist = Spree::Wishlist.new(:user => @user, :name => "My Wishlist")
   end
 
-  it "should be valid" do
-    @wishlist.should be_valid
+  context "creating a new wishlist" do
+    it "is valid with valid attributes" do
+      @wishlist.should be_valid
+    end
+
+    it "is not valid without a name" do
+      @wishlist.name = nil
+      @wishlist.should_not be_valid
+    end
+  end
+
+  context "#include?" do
+    before(:each) do
+      @variant = Factory(:variant)
+      wished_product = Spree::WishedProduct.new(:variant => @variant)
+      @wishlist.wished_products << wished_product
+      @wishlist.save
+    end
+
+    it "should be true if the wishlist includes the specified variant" do
+      @wishlist.include?(@variant.id).should be_true
+    end
+  end
+
+  context "#to_param" do
+    it "should return the wishlist's access_hash" do
+      @wishlist.to_param.should == @wishlist.access_hash
+    end
+  end
+
+  context "#can_be_read_by?" do
+    context "when the wishlist is private" do
+      before(:each) do
+        @wishlist.is_private = true
+      end
+
+      it "is true when the user owns the wishlist" do
+        @wishlist.can_be_read_by?(@user).should be_true
+      end
+
+      it "is false when the user does not own the wishlist" do
+        other_user = Factory(:user)
+        @wishlist.can_be_read_by?(other_user).should be_false
+      end
+    end
+
+    context "when the wishlist is public" do
+      it "is true for any user" do
+        @wishlist.is_private = false
+        other_user = Factory(:user)
+        @wishlist.can_be_read_by?(other_user).should be_true
+      end
+    end
+  end
+
+  context "#is_public?" do
+    it "is true when the wishlist is public" do
+      @wishlist.is_private = false
+      @wishlist.is_public?.should be_true
+    end
+
+    it "is false when the wishlist is private" do
+      @wishlist.is_private = true
+      @wishlist.is_public?.should_not be_true
+    end
   end
 end
