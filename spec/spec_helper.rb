cb066ec742a43c9495eb33daa85ed921d06048be
John Dyer <lbrapid@gmail.com>
2012-01-04 14:03:25 -0500

    Fixes failures on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,38 +1,19 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
+  # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
 end
 
-Spree::Zone.class_eval do
-  def self.global
-    find_by_name("GlobalZone") || Factory(:global_zone)
-  end
-end
-
-@configuration ||= Spree::AppConfiguration.find_or_create_by_name("Default configuration")
