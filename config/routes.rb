90353c02c9ba561f010fdc8740d1776effd51fd6
John Dyer <lbrapid@gmail.com>
2012-03-05 09:45:27 -0500

    Fixed routes not loading properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.prepend do
+Spree::Core::Engine.routes.append do
   resources :wishlists
   resources :wished_products, :only => [:create, :update, :destroy]
   match '/wishlist' => "wishlists#show", :as => "default_wishlist"
