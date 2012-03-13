a42ee22d13ccf71b56ad24ff9194448348056197
Michael Bianco <iloveitaly@gmail.com>
2012-03-13 15:59:12 -0400

    Removed override for email_to_wishlist. Style updates.
    * Added :class => 'button' to all action links
    * m/d/y on create_at header
    * Added some skeleton CSS classes here and there
    * Eliminated override for email_to_wishlist; added directly to wishlist/show instead

--- app/overrides/add_email_to_wishlist_footer_links.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/wishlists/show",
-                     :name => "add_email_to_wishlist_footer_links",
-                     :insert_after => "[data-hook='wishlist_footer_links']",
-                     :text => %(<% if respond_to?(:email_to_friend_url) %> <div id="email_to_friend">
-                       <%= link_to(t('email_to_friend.send_to_friend'), email_to_friend_url('wishlist', @wishlist)) %>
-                     </div><% end %>),
-                     :disabled => false)
