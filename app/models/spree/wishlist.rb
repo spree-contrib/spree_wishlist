dabbdc42e25fd59e4ac50c31a47751dd70ddb01a
salemine technologies <info@salemine.com>
2012-11-19 15:49:06 -0700

    make compatible with spree 1.2.0

--- app/models/spree/wishlist.rb
+++ app/models/spree/wishlist.rb
@@ -1,5 +1,6 @@
 class Spree::Wishlist < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, :class_name => Spree.user_class
+    
   has_many :wished_products
   before_create :set_access_hash
 
@@ -39,6 +40,8 @@ class Spree::Wishlist < ActiveRecord::Base
   private
 
   def set_access_hash
-    self.access_hash = Digest::SHA1.hexdigest("--#{user_id}--#{user.password_salt}--#{Time.now}--")
-  end
+    random_string = SecureRandom::hex(16)
+    self.access_hash = Digest::SHA1.hexdigest("--#{user_id}--#{random_string}--#{Time.now}--")
+  end  
+  
 end
