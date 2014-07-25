fbfb920a617cb9fd638109d4bea42e2d0a945e88
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2014-07-25 17:27:05 -0300

    Translate routes to pt-BR

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  resources :wishlists
+  resources :wishlists, path: "lista-de-desejos"
   resources :wished_products, only: [:create, :update, :destroy]
-  get '/wishlist' => "wishlists#default", as: "default_wishlist"
+  get '/lista-de-desejos' => "wishlists#default", as: "default_wishlist"
 end
