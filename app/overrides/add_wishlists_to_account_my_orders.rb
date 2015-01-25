7ddc9804e2b65c55c3775916a60d4207adebca7e
Tobias Bohwalli <hi@futhr.io>
2015-01-25 21:58:31 +0100

    Demove Deface option "disabled" from overrides.

--- app/overrides/add_wishlists_to_account_my_orders.rb
+++ app/overrides/add_wishlists_to_account_my_orders.rb
@@ -1,6 +1,7 @@
-Deface::Override.new(virtual_path: 'spree/users/show',
-                     name: 'add_wishlists_to_account_my_orders',
-                     insert_after: "[data-hook='account_my_orders']",
-                     partial: 'spree/users/wishlists',
-                     disabled: false,
-                     original: 'f1f0e9b7901295ea2f4dedaa53efd632aaa2d26e')
+Deface::Override.new(
+  virtual_path: 'spree/users/show',
+  name: 'add_wishlists_to_account_my_orders',
+  insert_after: "[data-hook='account_my_orders']",
+  partial: 'spree/users/wishlists',
+  original: 'f1f0e9b7901295ea2f4dedaa53efd632aaa2d26e'
+)
