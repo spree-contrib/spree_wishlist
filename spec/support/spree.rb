ff16837174a5ee859b2edb223c72707743091016
Tobias Bohwalli <hi@futhr.io>
2015-02-12 07:48:42 +0100

    Fixes spree spec support.

--- spec/support/spree.rb
+++ spec/support/spree.rb
@@ -4,6 +4,6 @@ require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 
 RSpec.configure do |config|
-  config.include Spree::TestingSupport::ControllerRequests
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
   config.include Spree::TestingSupport::UrlHelpers
 end
