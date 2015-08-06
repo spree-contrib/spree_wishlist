8e64f7c45478ae9805ca68049243c64813ff3a35
treyking11 <treyking11@cox.net>
2015-08-06 15:15:18 -0700

    Added method to display total price in wishlist

--- app/models/spree/wished_product.rb
+++ app/models/spree/wished_product.rb
@@ -1,4 +1,13 @@
 class Spree::WishedProduct < ActiveRecord::Base
   belongs_to :variant
   belongs_to :wishlist
+
+  def total
+    quantity * variant.price
+  end
+
+  def display_total
+    Spree::Money.new(total)
+  end
+
 end
