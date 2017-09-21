3c31c98c90f956e7bde6bddfde7c22b1c95216d1
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-09-21 13:03:23 +0200

    update to support rails 5.1

--- db/migrate/20140403013413_remove_wished_products_for_deleted_variants.rb
+++ db/migrate/20140403013413_remove_wished_products_for_deleted_variants.rb
@@ -1,4 +1,4 @@
-class RemoveWishedProductsForDeletedVariants < ActiveRecord::Migration
+class RemoveWishedProductsForDeletedVariants < SpreeExtension::Migration[4.2]
   def up
     Spree::WishedProduct.includes(:variant).find_each do |wished_product|
       wished_product.destroy unless wished_product.variant
--- db/migrate/20150805204533_add_quantity_to_wished_products.rb
+++ db/migrate/20150805204533_add_quantity_to_wished_products.rb
@@ -1,4 +1,4 @@
-class AddQuantityToWishedProducts < ActiveRecord::Migration
+class AddQuantityToWishedProducts < SpreeExtension::Migration[4.2]
   def change
     add_column :spree_wished_products, :quantity, :integer, null: false, default: 1
   end
