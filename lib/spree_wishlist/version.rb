4d6bfd606ec633d78d3f4a2902282543881aea80
Damian Legawiec <damian@sparksolutions.co>
2020-11-04 15:13:27 +0100

    Bump version to 3.1.0

--- lib/spree_wishlist/version.rb
+++ lib/spree_wishlist/version.rb
@@ -9,8 +9,8 @@ module SpreeWishlist
 
   module VERSION
     MAJOR = 3
-    MINOR = 0
-    TINY  = 1
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
